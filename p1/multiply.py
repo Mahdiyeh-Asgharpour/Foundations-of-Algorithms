e6a8b6d612fb591b79cd1d77e10d458644513f86
Mahdiyeh <www.mahdiyeh.mazani@gmail.com>
2023-03-12 22:25:33 +0330

    Update multiply.py

--- p1/multiply.py
+++ p1/multiply.py
@@ -6,6 +6,7 @@ n2=input("number 2:")
 array1=[]
 array2=[]
 carry=[]
+answer=[]
 #at first all of element in carry is 0 
 if(n1.__len__()>=n2.__len__()):
     for i in range(n1.__len__()):
@@ -14,7 +15,23 @@ else:
     for i in range(n2.__len__()):
         carry.append(0)
 for i in range(n1.__len__()):
-    array1.append(n1)
+    array1.append(n1[i])
 for i in range(n2.__len__()):
-    array2.append(n2)
-
+    array2.append(n2[i])
+def Multiply(n):
+    while(n>=0):
+    #answer multiply
+        multi==array1[n]*array2[n]
+        if(multi>9):
+            carry.insert(n+1, multi/10)
+        if(n1.__len__()>=n2.__len__()):
+            if(n==0 or n==n1.__len__()):
+                answer.append(multi+int(carry[n]))
+        else:
+            if(n==0 or n==n2.__len__()):
+                answer.append(multi+int(carry[n]))
+        multiply(n-1)
+if(n1.__len__()>=n2.__len__()):
+    Multiply(n1.__len__())
+else:
+    Multiply(n2.__len__())
