095e165815aafeadbf881b858a1e1cd002edaa68
Mahdiyeh <www.mahdiyeh.mazani@gmail.com>
2023-05-17 22:11:38 +0330

    Add files via upload

--- p3/greedy.py
+++ p3/greedy.py
@@ -2,7 +2,7 @@ x=input()
 y=input()
 xx=x.split()
 yy=y.split()
-
+arr=[]
  
 # Returns min number of squares needed
 def minimumSquare(a, b):
@@ -20,13 +20,14 @@ def minimumSquare(a, b):
      
         # Update result
         result += int(a / b)
- 
+        q=int(a/b)
         rem = int(a % b)
         a = b
         b = rem
- 
+        f=f"{q} x ( {a} x {a} ) km^2"
+        arr.append(f)
+    for item in arr:
+        print(item)
     return result
- 
-
-print(minimumSquare(int(xx[0]), int(yy[0])))
- 
+    
+print(minimumSquare(int(xx[0]),int (yy[0])))
